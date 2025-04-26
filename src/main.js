0e98a8d303d95b9b0402ed09398857b86f1e73ef
Nina-zl <sidorchenkonatasha@gmail.com>
2025-04-26 16:38:03 +0300

    adds FAQ markup

--- src/main.js
+++ src/main.js
@@ -3,3 +3,9 @@ import './scripts/theme.module.js';
 import './scripts/themeButton.js';
 import './scripts/config.js';
 import './scripts/mobile-menu.js';
+
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+new Accordion('.accordion-container');
+
