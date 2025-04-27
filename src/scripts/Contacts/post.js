139c65593bb0452242127353c4835325a9f94eb8
Taty <taty.bystrova@gmail.com>
2025-04-27 19:30:52 +0300

    add svg to input email

--- src/scripts/Contacts/post.js
+++ src/scripts/Contacts/post.js
@@ -4,42 +4,47 @@ const FORM = document.querySelector('.work-contact');
 const EMAIL = document.querySelector('.email-input');
 const LABEL = document.querySelector('.email-label');
 
+
 function validateEmail(email) {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return regex.test(email);
 }
 
 EMAIL.addEventListener('input', () => {
-    const email = FORM.elements.email.value;
+    //const email = FORM.elements.email.value;//
+    const value = EMAIL.value.trim();
 
-    if (validateEmail(email) === false) {
+    if (validateEmail(value)) {
+        EMAIL.style.color = 'black';
+        EMAIL.classList.remove('error');
+        EMAIL.classList.add('done');
+        LABEL.classList.add('success');
+  } else {
+        EMAIL.style.color = 'red';
         EMAIL.classList.add('error');
         EMAIL.classList.remove('done');
         LABEL.classList.remove('success');
-    } else {
-        EMAIL.classList.add('done');
-        LABEL.classList.add('success');
-        EMAIL.classList.remove('error');
-    }
+  }
 });
+
 EMAIL.addEventListener('blur', () => {
-    EMAIL.classList.remove('done');
-    EMAIL.classList.remove('error');
-    LABEL.classList.remove('success');
+  EMAIL.classList.remove('done');
+  EMAIL.classList.remove('error');
+  EMAIL.style.color = 'black';
+  ICON.innerHTML = '';
+  ICON.style.display = 'none';
+  LABEL.classList.remove('success');
 });
 
 FORM.addEventListener('submit', async event => {
-    event.preventDefault();
-    const DATA = {
-        email: FORM.elements.email.value,
-        comment: FORM.elements.message.value.trim(),
-    };
-    try {
-        const result = await requestsPost(DATA, FORM);
-    } catch (error) {
-        console.error('Помилка при надсиланні форми:', error);
-    }
+  event.preventDefault();
+  const DATA = {
+    email: FORM.elements.email.value,
+    comment: FORM.elements.textarea.value.trim(),
+  };
+  try {
+    const result = await requestsPost(DATA, FORM);
+  } catch (error) {
+    console.error('Помилка при надсиланні форми:', error);
+  }
 });
-
-
-    
