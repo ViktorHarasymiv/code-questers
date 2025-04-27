37ad69998405106bd8b22ffce7e62cb3fe04c1a2
Taty <taty.bystrova@gmail.com>
2025-04-27 09:27:49 +0300

    add foldre Contacts add 3 files js

---
+++ src/scripts/Contacts/post.js
@@ -0,0 +1,43 @@
+import { requestsPost } from './request-post';
+
+const FORM = document.querySelector('.work-contact');
+const EMAIL = document.querySelector('.email-input');
+const LABEL = document.querySelector('.email-label');
+
+function validateEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+EMAIL.addEventListener('input', () => {
+    const email = FORM.elements.email.value;
+
+    if (validateEmail(email) === false) {
+        EMAIL.classList.add('error');
+        EMAIL.classList.remove('done');
+
+        LABEL.classList.remove('success');
+    } else {
+        EMAIL.classList.add('done');
+        LABEL.classList.add('success');
+        EMAIL.classList.remove('error');
+    }
+});
+EMAIL.addEventListener('blur', () => {
+    EMAIL.classList.remove('done');
+    EMAIL.classList.remove('error');
+    LABEL.classList.remove('success');
+});
+
+FORM.addEventListener('submit', async event => {
+    event.preventDefault();
+    const DATA = {
+        email: FORM.elements.email.value,
+    };
+    try {
+        const result = await requestsPost(DATA, FORM);
+    } catch (error) { }
+});
+
+
+    
